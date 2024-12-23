61bb1f2d592d11f60cd18c9c96be97da743d9dec
connor <connorflanigan@gmail.com>
2024-12-23 17:37:10 -0500

    First pass at all header bindings.

---
+++ wrapper.h
@@ -0,0 +1,8 @@
+#include <dvdread/dvd_reader.h>
+#include <dvdread/bitreader.h>
+#include <dvdread/dvd_udf.h>
+#include <dvdread/ifo_print.h>
+#include <dvdread/ifo_read.h>
+#include <dvdread/nav_print.h>
+#include <dvdread/nav_read.h>
+#include <dvdread/nav_types.h>
